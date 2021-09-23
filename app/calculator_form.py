8fc176168bb70fa2eadc891d9a013bc5123b0d4f
Shenyi Hu <shuu0020@student.monash.edu>
2021-09-23 15:11:08 +1000

    Added validation for user inputs.

--- app/calculator_form.py
+++ app/calculator_form.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, DateField, TimeField
 from wtforms.validators import DataRequired, ValidationError, Optional
 
+
 # validation for form inputs
 class Calculator_Form(FlaskForm):
     # this variable name needs to match with the input attribute name in the html file
@@ -20,31 +21,60 @@ class Calculator_Form(FlaskForm):
         if field.data is None:
             raise ValidationError('Field data is none')
         elif field.data == '':
-            raise ValueError("cannot fetch data")
+            raise ValueError("Cannot fetch data")
+        elif field.data < 5 or field.data > 150:
+            raise ValueError("Please enter a valid battery capacity")
 
     # validate initial charge here
     def validate_InitialCharge(self, field):
         # another example of how to compare initial charge with final charge
         # you may modify this part of the code
-        if field.data > self.FinalCharge.data:
+        if field.data is None:
+            raise ValidationError('Field data is none')
+        elif field.data == '':
+            raise ValueError("Cannot fetch data")
+        elif field.data > self.FinalCharge.data or field.data < 0 or field.data > 99:
             raise ValueError("Initial charge data error")
 
     # validate final charge here
     def validate_FinalCharge(self, field):
-        pass
+        if field.data is None:
+            raise ValidationError('Field data is none')
+        elif field.data == '':
+            raise ValueError("Cannot fetch data")
+        elif field.data < self.InitialCharge.data or field.data > 100 or field.data < 1:
+            raise ValueError("Final charge data error")
 
     # validate start date here
     def validate_StartDate(self, field):
-        pass
+        if field.data is None:
+            raise ValidationError('Field data is none')
+        elif field.data == '':
+            raise ValueError("Cannot fetch data")
+        elif field.data < '01/07/2008':
+            raise ValueError('Please enter a date from 01/07/2008 onwards')
 
     # validate start time here
     def validate_StartTime(self, field):
-        pass
+        if field.data is None:
+            raise ValidationError('Field data is none')
+        elif field.data == '':
+            raise ValueError("Cannot fetch data")
 
     # validate charger configuration here
     def validate_ChargerConfiguration(self, field):
-        pass
+        if field.data is None:
+            raise ValidationError('Field data is none')
+        elif field.data == '':
+            raise ValueError("Cannot fetch data")
+        elif field.data < 1 or field.data > 8:
+            raise ValueError('Please enter a configuration number')
 
     # validate postcode here
     def validate_PostCode(self, field):
-        pass
+        if field.data is None:
+            raise ValidationError('Field data is none')
+        elif field.data == '':
+            raise ValueError("Cannot fetch data")
+        elif field.data > 9999 or field.data < 800:
+            raise ValueError("Please enter a valid Australian post code")
