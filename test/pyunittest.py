3accd154aa35f6aee2b535064e25ac2f9529670c
Shenyi Hu <shuu0020@student.monash.edu>
2021-09-26 19:09:38 +1000

    Modified the block comment for each test case for better readability in the test strategy documentation.

--- test/pyunittest.py
+++ test/pyunittest.py
@@ -5,126 +5,129 @@ import unittest
 class TestCalculator(unittest.TestCase):
 
     def test_get_price_and_power(self):
-        # Test case for charger configuration 1
+        # Test case 1: Charger configuration 1
         calculator = Calculator("100", "50", "100", "01/02/2021", "00:00", "1", "3168")
         calculator.get_price_and_power()
         self.assertEqual(calculator.base_price, 5)
         self.assertEqual(calculator.power, 2)
 
-        # Test case for charger configuration 2
+        # Test case 2: Charger configuration 2
         calculator.charger_configuration = 2
         calculator.get_price_and_power()
         self.assertEqual(calculator.base_price, 7.5)
         self.assertEqual(calculator.power, 3.6)
 
-        # Test case for charger configuration 3
+        # Test case 3: Charger configuration 3
         calculator.charger_configuration = 3
         calculator.get_price_and_power()
         self.assertEqual(calculator.base_price, 10)
         self.assertEqual(calculator.power, 7.2)
 
-        # Test case for charger configuration 4
+        # Test case 4: Charger configuration 4
         calculator.charger_configuration = 4
         calculator.get_price_and_power()
         self.assertEqual(calculator.base_price, 12.5)
         self.assertEqual(calculator.power, 11)
 
-        # Test case for charger configuration 5
+        # Test case 5: Charger configuration 5
         calculator.charger_configuration = 5
         calculator.get_price_and_power()
         self.assertEqual(calculator.base_price, 15)
         self.assertEqual(calculator.power, 22)
 
-        # Test case for charger configuration 6
+        # Test case 6: Charger configuration 6
         calculator.charger_configuration = 6
         calculator.get_price_and_power()
         self.assertEqual(calculator.base_price, 20)
         self.assertEqual(calculator.power, 36)
 
-        # Test case for charger configuration 7
+        # Test case 7: Charger configuration 7
         calculator.charger_configuration = 7
         calculator.get_price_and_power()
         self.assertEqual(calculator.base_price, 30)
         self.assertEqual(calculator.power, 90)
 
-        # Test case for charger configuration 8
+        # Test case 8: Charger configuration 8
         calculator.charger_configuration = 8
         calculator.get_price_and_power()
         self.assertEqual(calculator.base_price, 50)
         self.assertEqual(calculator.power, 350)
 
-        # Test case for an invalid charger configuration smaller than 1
+        # Test case 9: An invalid charger configuration smaller than 1
         calculator.charger_configuration = 0
         self.assertRaises(Exception, calculator.get_price_and_power())
 
-        # Test case for an invalid charger configuration greater than 8
+        # Test case 10: An invalid charger configuration greater than 8
         calculator.charger_configuration = 9
         self.assertRaises(Exception, calculator.get_price_and_power())
 
     def test_is_holiday(self):
-        # Test case for ACT (Canberra Day)
+        # Test case 11: ACT (Canberra Day)
         calculator = Calculator("100", "50", "100", "08/03/2021", "02:00", "1", "2601")
         self.assertTrue(calculator.is_holiday())
 
-        # Test case for NSW (Bank Holiday)
+        # Test case 12: NSW (Bank Holiday)
         calculator = Calculator("100", "50", "100", "02/08/2021", "02:00", "1", "2666")
         self.assertTrue(calculator.is_holiday())
 
-        # Test case for VIC (AFL Grand Final)
+        # Test case 13: VIC (AFL Grand Final)
         calculator = Calculator("100", "50", "100", "24/09/2021", "02:00", "1", "3123")
         self.assertTrue(calculator.is_holiday())
 
-        # Test case for QLD (Royal Queensland Show)
+        # Test case 14: QLD (Royal Queensland Show)
         calculator = Calculator("100", "50", "100", "11/08/2021", "02:00", "1", "4022")
         self.assertTrue(calculator.is_holiday())
 
-        # Test case for SA (Adelaide Cup Day)
+        # Test case 15: SA (Adelaide Cup Day)
         calculator = Calculator("100", "50", "100", "08/03/2021", "02:00", "1", "5555")
         self.assertTrue(calculator.is_holiday())
 
-        # Test case for WA (Anzac Day, Additional)
+        # Test case 16: WA (Anzac Day, Additional)
         calculator = Calculator("100", "1", "100", "26/04/2021", "02:00", "1", "6666")
         self.assertTrue(calculator.is_holiday())
 
-        # Test case for TAS (Eight Hours Day)
+        # Test case 17: TAS (Eight Hours Day)
         calculator = Calculator("100", "1", "100", "08/03/2021", "02:00", "1", "7777")
         self.assertTrue(calculator.is_holiday())
 
-        # Test case for a non-holiday
+        # Test case 18: A non-holiday
         calculator = Calculator("100", "1", "100", "21/09/2021", "02:00", "1", "3168")
         self.assertFalse(calculator.is_holiday())
 
     def test_is_weekday(self):
-        # Test case for Monday
+        # Test case 19: Monday
         calculator = Calculator("66", "1", "98", "20/09/2021", "06:28", "1", "1234")
         self.assertTrue(calculator.is_weekday())
 
-        # Test case for Tuesday
+        # Test case 20: Tuesday
         calculator = Calculator("66", "1", "98", "21/09/2021", "06:28", "1", "1234")
         self.assertTrue(calculator.is_weekday())
 
-        # Test case for Wednesday
+        # Test case 21: Wednesday
         calculator = Calculator("66", "1", "98", "22/09/2021", "06:28", "1", "1234")
         self.assertTrue(calculator.is_weekday())
 
-        # Test case for Thursday
+        # Test case 22: Thursday
         calculator = Calculator("66", "1", "98", "23/09/2021", "06:28", "1", "1234")
         self.assertTrue(calculator.is_weekday())
 
-        # Test case for Friday
+        # Test case 23: Friday
         calculator = Calculator("66", "1", "98", "24/09/2021", "06:28", "1", "1234")
         self.assertTrue(calculator.is_weekday())
 
-        # Test case for Saturday
+        # Test case 24: Saturday
         calculator = Calculator("66", "1", "98", "25/09/2021", "06:28", "1", "1234")
         self.assertFalse(calculator.is_weekday())
 
-        # Test case for Sunday
+        # Test case 25: Sunday
         calculator = Calculator("66", "1", "98", "26/09/2021", "06:28", "1", "1234")
         self.assertFalse(calculator.is_weekday())
 
-    def test_cost(self):
-        # Example from spec
+    def test_time_calculation(self):
+        pass
+
+    def test_cost_calculation(self):
+        # Just a random example from spec, to be rewritten
         calculator = Calculator("82", "20", "80", "21/09/2021", "14:30", "8", "3168")
         self.assertAlmostEqual(float(calculator.cost_calculation()), 27.06)
 
