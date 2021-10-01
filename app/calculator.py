2caa2fd0c6744928f62a8049fcda1a61943fbcc6
mgam0010 <mgam0010@student.monash.edu>
2021-10-02 00:19:22 +0530

    removed useless methods fixed cost calculation equation implemented req3 algorithm

--- app/calculator.py
+++ app/calculator.py
@@ -102,7 +102,6 @@ class Calculator:
         else:
             province = 'ACT'
         aus_state_holidays = holidays.CountryHoliday('AUS', prov=province, state=None)
-        print(aus_state_holidays)
         return self.start_datetime in aus_state_holidays
 
     def is_weekday(self):
@@ -118,18 +117,22 @@ class Calculator:
     def calculate_cost(self):
         date_now = datetime.now()
         start_date = self.start_datetime
-        time = self.start_datetime
+        total_cost = 0
+        # if date is in the past
         if start_date < date_now:
             timedates = self.get_charging_times(start_date)
-            print(timedates)
             for timedate in timedates:
-                self.calculate_cost_hour(timedate)
+                total_cost += self.calculate_cost_hour(timedate)
+        # if date is in the future
         else:
             past_dates = self.get_ref_dates(start_date)
-            print(past_dates)
             for date_time in past_dates:
-                print(date_time)
-                print(self.get_charging_times(date_time))
+                cost = 0
+                timedates = self.get_charging_times(date_time)
+                for timedate in timedates:
+                    cost += self.calculate_cost_hour(timedate, True)
+                total_cost += cost
+        return total_cost
 
     def get_ref_dates(self, date):
         while date > datetime.now():
@@ -142,7 +145,6 @@ class Calculator:
         start_datetime = date
         date = start_datetime.date()
         charging_time = self.time_calculation()/60
-        print(charging_time)
         times = []
         hour = start_datetime.time()
         mins = (60 - start_datetime.minute) / 60
@@ -166,12 +168,12 @@ class Calculator:
         self.is_holiday()
         sate = self.get_state_code()
         aus_state_holidays = holidays.CountryHoliday('AUS', prov=sate, state=None)
-        print(aus_state_holidays)
         return timedata[0] in aus_state_holidays
 
     def is_weekday_p_hour(self, timedate):
         """Check if the this date is a weekday"""
         return timedate[0].weekday() <= 4
+
     def is_peak_p_hour(self, timedate):
         """Check if this time peak hour"""
         FMT = '%H:%M:%S'
@@ -179,10 +181,11 @@ class Calculator:
         upper = datetime.strptime("18:00:00", FMT).time()
         return lower <= timedate[1] <= upper
 
-    def calculate_cost_hour(self, timedate):
-        print(timedate)
-        print(self.is_holiday_p_hour(timedate))
-        print(self.is_weekday_p_hour(timedate))
+    def calculate_cost_hour(self, timedate, cloudcover=False):
+        if cloudcover:
+            cc_value = self.get_cloud_cover(timedate)
+        else:
+            cc_value = 0
         if self.is_holiday_p_hour(timedate) or self.is_weekday_p_hour(timedate):
             surcharge_factor = 1.1
         else:
@@ -195,15 +198,16 @@ class Calculator:
             self.get_price_and_power()
             dl = self.get_day_light_length(timedate[0])
             si = self.get_sun_hour(timedate[0])
-            solar_energy = si * (timedate[2]/dl) * 50 * 0.2
+            solar_energy = si * (timedate[2]/dl)*(1-(cc_value/100)) * 50 * 0.2
             charge_energy = self.power * timedate[2]
             net_energy = charge_energy - solar_energy
             if net_energy < 0:
                 net_energy = 0
-            cost = (self.final_charge - self.initial_charge) / 100 * net_energy * self.base_price / 100 * surcharge_factor * discount_factor
+            cost = net_energy * (self.base_price / 100) * surcharge_factor * discount_factor
         else:
             charge_energy = self.power * timedate[2]
-            cost = (self.final_charge - self.initial_charge) / 100 * charge_energy * self.base_price / 100 * surcharge_factor * discount_factor
+
+            cost = charge_energy * (self.base_price / 100) * surcharge_factor * discount_factor
 
         return cost
 
@@ -224,40 +228,11 @@ class Calculator:
         stateJson = self.get_weather_data(date)
         return stateJson["sunHours"]
 
-    def get_solar_energy_duration(self, date):
-        """ returns total hours of solar generation in hours while charging"""
-        start_time = self.start_datetime
-        weather_data = self.get_weather_data(date)
-        FMT = '%H:%M:%S'
-        sunrise_time = datetime.strptime(weather_data["sunrise"], FMT)
-        sunset_time = datetime.strptime(weather_data["sunset"], FMT)
-        charging_time = timedelta(minutes=self.time_calculation())
-        final_time = self.start_datetime + charging_time
-        print(sunrise_time)
-        print(sunset_time)
-        print(start_time)
-        print(final_time.time())
-        if start_time.time() < sunrise_time.time() and final_time <= sunset_time:
-            du = (final_time - sunrise_time)
-            print("1")
-        elif start_time.time() >= sunrise_time.time() and final_time.time() <= sunset_time.time():
-            du = charging_time
-            print("2")
-        elif start_time.time() >= sunrise_time.time() and final_time.time() > sunset_time.time():
-            du = sunset_time - start_time
-            print("3")
-        elif start_time.time() < sunrise_time.time() and final_time.time() > sunset_time.time():
-            du = self.get_day_light_length()
-            print("4")
-        print(du)
-        return du.seconds / 3600
-
     """ 
      Returns  day light hours for a specific date in a state
      Output:
          float of hours of daylight for this date
     """
-
     def get_day_light_length(self, date):
         stateJson = self.get_weather_data(date)
         sunrise_time = stateJson["sunrise"]
@@ -272,9 +247,7 @@ class Calculator:
     Output:
         array list [0..23] with the cloud cover value at the hour index
     """
-
     def get_cloud_cover(self, date):
-
         stateJson = self.get_weather_data(date)
         hourly_history = stateJson["hourlyWeatherHistory"]
         cc_per_hour = []
@@ -282,67 +255,6 @@ class Calculator:
             cc_per_hour.append(hourly_history[x]["cloudCoverPct"])
         return cc_per_hour
 
-    def calculate_solar_energy(self):
-        """ Get solar energy generated """
-        date = datetime.strftime(self.start_datetime, "%Y-%m-%d")
-        si = self.get_sun_hour(date)
-        dl = self.get_day_light_length(date)
-        du = self.get_solar_energy_duration(date)
-        e = si * (du / dl) * 50 * 0.2
-        return e
-
-    def calculate_solar_energy_future(self, date):
-        ref_dates = self.get_ref_dates(date)
-        total = [0] * 3
-        index = 0
-        for i in ref_dates:
-            total[index] = self.calculate_solar_energy_w_cc(str(i))
-            index += 1
-        return total
-
-
-    def get_solar_charging_time(self):
-        start_datetime = self.start_datetime
-        date = self.start_datetime.date()
-        solar_du = self.get_solar_energy_duration(str(date))
-        times = []
-        hour = start_datetime.hour
-        mins = start_datetime.minute / 60
-        times.append([date, hour, mins])
-
-        for i in range(1, math.trunc(solar_du)):
-            date_time = (start_datetime + timedelta(hours=i * 1))
-            hour = date_time.hour
-            date = date_time.date()
-            times.append([date, hour, 1])
-
-        time = (start_datetime + timedelta(hours=math.trunc(solar_du), minutes=solar_du % 1))
-        hour = time.hour
-        mins = time.minute / 60
-        date = time.date()
-        times.append([date, hour, mins])
-        return times
-
-    def calculate_solar_energy_w_cc(self, date):
-        """ Get solar energy generated with cloud cover"""
-        this_date = date[0]
-        hour = date[1]
-        du = date[2]
-        si = self.get_sun_hour(str(this_date))
-        dl = self.get_day_light_length(str(this_date))
-        cc_list = self.get_cloud_cover(str(this_date))
-        cc = cc_list[hour]
-        e = si * (du / dl) * (1 - (cc / 100)) * 50 * 0.2
-        return e
-
-
-
-    def get_solar_charges(self):
-        times = self.solar_charging_time
-        solar_charge = []
-        for time in times:
-            solar_charge.append(self.calculate_solar_energy_w_cc(time))
-        return solar_charge
 
     def get_state_id(self):
         """Get state id for any state code"""
@@ -366,41 +278,14 @@ class Calculator:
 
     def get_weather_data(self, *date):
         """Get json for state and  date from api"""
-
         if len(date) == 0:
             start_date = datetime.strftime(self.start_datetime, "%Y-%m-%d")
         else:
-            print("date")
-            print(date)
-            print(date[0])
             start_date = str(date[0])
-            print(start_date[0])
         state_id = self.get_state_id()
         requestURL = "http://118.138.246.158/api/v1/weather?location=" + state_id + "&date=" + start_date
         response = requests.get(requestURL)
         return response.json()
-
-    def is_holiday_(self, date):
-        """Check if the charging start date is a holiday in the state represented by postcode"""
-        # Get state from post code
-        if 800 <= self.post_code < 1000:
-            province = 'NT'
-        elif 1000 <= self.post_code < 2600 or 2619 <= self.post_code < 2900 or 2921 <= self.post_code < 3000:
-            province = 'NSW'
-        elif 3000 <= self.post_code < 4000 or 8000 <= self.post_code < 9000:
-            province = 'VIC'
-        elif 4000 <= self.post_code < 5000 or 9000 <= self.post_code < 10000:
-            province = 'QLD'
-        elif 5000 <= self.post_code < 6000:
-            province = 'SA'
-        elif 6000 <= self.post_code < 7000:
-            province = 'WA'
-        elif 7000 <= self.post_code < 8000:
-            province = 'TAS'
-        else:
-            province = 'ACT'
-        aus_state_holidays = holidays.CountryHoliday('AUS', prov=province, state=None)
-        return self.start_datetime in aus_state_holidays
 
     def req2(self):
         start_date = self.start_datetime
@@ -415,7 +300,9 @@ class Calculator:
 
 
 if __name__ == '__main__':
-    calculator = Calculator("100", "85", "100", "28/09/2020", "17:15", "1", "3800")
-    print(calculator.calculate_cost())
+    calculator = Calculator("100", "20", "100", "25/12/2020", "08:00", "4", "6001")
+    print(calculator.get_weather_data("2020-12-25"))
+    print(calculator.time_calculation())
+    print(calculator.req2())
 
 
